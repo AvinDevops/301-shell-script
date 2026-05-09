74fe7e1117a7e2ecf6574ef5ee9d07bfceb97937
AvinDevops <avinashbandela73@gmail.com>
2026-05-09 15:37:14 +0530

    with variables

---
+++ 03-with-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Avinash
+PERSON2=Kumar
+
+echo "$PERSON1::Hello ${PERSON2}, How are you?"
+echo "$PERSON2::I am doing good, what abt you!"
+echo "$PERSON1::I am good, how is your job!"
+echo "${PERSON2}::not bad, I am upgrading to Devops"
