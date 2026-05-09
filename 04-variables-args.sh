0a8e7ff79f834e57a7ede2e858566ce2ae8bd651
AvinDevops <avinashbandela73@gmail.com>
2026-05-09 15:42:31 +0530

    variables args

---
+++ 04-variables-args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::Hello ${PERSON2}, How are you?"
+echo "$PERSON2::I am doing good, what abt you!"
+echo "$PERSON1::I am good, how is your job!"
+echo "${PERSON2}::not bad, I am upgrading to Devops"
