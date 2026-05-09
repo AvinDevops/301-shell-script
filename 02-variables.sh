fd1800cee0ddac94f6b8e44ceb40fc35482ffa24
AvinDevops <avinashbandela73@gmail.com>
2026-05-09 15:32:40 +0530

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Avinash::Hello Kumar, How are you?"
+echo "Kumar::I am doing good, what abt you!"
+echo "Avinash::I am good, how is your job!"
+echo "Kumar::not bad, I am upgrading to Devops"
