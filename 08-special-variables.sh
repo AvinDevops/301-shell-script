4a48f6d345751294371ad70585b1e922df56bb9c
AvinDevops <avinashbandela73@gmail.com>
2026-05-09 16:25:18 +0530

    special variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -8,3 +8,5 @@ echo "Home directory of current user: $HOME"
 echo "which user is running this script: $USER"
 echo "Hostname is: $HOSTNAME"
 echo "process ID of the current shell script: $$"
+sleep 60 &
+echo "process ID of last background command: $!"
