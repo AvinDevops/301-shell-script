b63fa190d153fbeda213381aa826e0f47446c96d
AvinDevops <avinashbandela73@gmail.com>
2026-05-09 16:00:37 +0530

    variable array

--- 06-array.sh
+++ 06-array.sh
@@ -2,7 +2,7 @@
 
 FULLNAME=("Avinash" "Kumar" "Bandela")
 
-echo "first name is: $FULLNAME[0]"
-echo "middle name is: $FULLNAME[1]"
-echo "last name is: $FULLNAME[2]"
-echo "full name is: $FULLNAME[@]"
+echo "first name is: ${FULLNAME[0]}"
+echo "middle name is: ${FULLNAME[1]}"
+echo "last name is: ${FULLNAME[2]}"
+echo "full name is: ${FULLNAME[@]}"
