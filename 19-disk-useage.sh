f08c33703f7be7861eb6279c252cd9081d65aa41
AvinDevops <avinashbandela73@gmail.com>
2026-05-11 16:50:15 +0530

    disk useage

--- 19-disk-useage.sh
+++ 19-disk-useage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DISK_USEAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=6
-MESSAGE=""
+# MESSAGE=""
 
 while IFS= read -r line
 do
@@ -10,6 +10,8 @@ do
    if [ $USEAGE -ge $DISK_THRESHOLD ]
    then
         # echo "Folder:$FOLDER usage:$USEAGE is greater than $DISK_THRESHOLD"
-        MESSAGE+="Folder:$FOLDER usage:$USEAGE is greater than $DISK_THRESHOLD"
+        MESSAGE="Folder:$FOLDER usage:$USEAGE is greater than $DISK_THRESHOLD"
    fi
 done <<< $DISK_USEAGE
+
+echo "Message is: $MESSAGE"
